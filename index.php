0ca55ac8ab1cc3376d2e6083238fb69f4fc9751c
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-05-16 23:07:28 +0200

    Feat: just the base

--- index.php
+++ index.php
@@ -9,4 +9,37 @@
     - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
     */
 
+    class Movie {
+        public $title;
+        public $director;
+        public $genre;
+
+        function __construct($_title, $_director, $_genre) {
+            $this->title = $_title;
+            $this->director = $_director;
+            $this->genre = $_genre; 
+        }
+
+        public function getMovieDetails() {
+            return $this->title . ' - Regista: ' . $this->director . ' - Genere: ' . $this->genre;
+        }
+    }
+
+    $eva = new Movie('Evangelion: 1.0 You Are (Not) Alone', 'Hideaki Anno', 'Anime');
+    //var_dump($eva);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Oop</title>
+</head>
+<body>
+    <div>
+        Film: <?php echo $eva->getMovieDetails(); ?>
+    </div>
+</body>
+</html>
